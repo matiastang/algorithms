1089301f463588685a41179ba828665e862f4781
matiastang <18380449615@163.com>
2021-02-24 10:30:57 +0800

    feat: - 617. 合并二叉树,104. 二叉树的最大深度

---
+++ TS/LeetCode/树/LC_104.ts
@@ -0,0 +1,138 @@
+/*
+ * @Author: tangdaoyong
+ * @Date: 2021-02-24 09:17:39
+ * @LastEditors: tangdaoyong
+ * @LastEditTime: 2021-02-24 10:17:37
+ * @Description: 104. 二叉树的最大深度
+ */
+/*
+104. 二叉树的最大深度
+给定一个二叉树，找出其最大深度。
+
+二叉树的深度为根节点到最远叶子节点的最长路径上的节点数。
+
+说明: 叶子节点是指没有子节点的节点。
+
+示例：
+给定二叉树 [3,9,20,null,null,15,7]，
+
+    3
+   / \
+  9  20
+    /  \
+   15   7
+返回它的最大深度 3 。
+*/
+// class TreeNode {
+//     val: number
+//     left: TreeNode | null
+//     right: TreeNode | null
+//     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+//         this.val = (val===undefined ? 0 : val)
+//         this.left = (left===undefined ? null : left)
+//         this.right = (right===undefined ? null : right)
+//     }
+// }
+/**
+ * @description: 二叉树的最大深度，深度优先搜索DFS
+ * @param {TreeNode} root
+ * @return {*}
+ * 108 ms	42 MB	TypeScript
+ * 复杂度分析
+时间复杂度：O(n)O(n)，其中 nn 为二叉树节点的个数。每个节点在递归中只被遍历一次。
+空间复杂度：O(\textit{height})O(height)，其中 \textit{height}height 表示二叉树的高度。递归函数需要栈空间，而栈空间取决于递归的深度，因此空间复杂度等价于二叉树的高度。
+ */
+function maxDepthOne(root: TreeNode | null): number {
+    if (root === null) {
+        return 0
+    }
+    return Math.max(maxDepthOne(root.left), maxDepthOne(root.right)) + 1
+};
+/**
+ * @description: 二叉树的最大深度，广度优先搜索BFS
+ * @param {TreeNode} root
+ * @return {*}
+ * 108 ms	42.2 MB	TypeScript
+ * 复杂度分析
+时间复杂度：O(n)O(n)，其中 nn 为二叉树的节点个数。与方法一同样的分析，每个节点只会被访问一次。
+空间复杂度：此方法空间的消耗取决于队列存储的元素数量，其在最坏情况下会达到 O(n)O(n)。
+ */
+function maxDepthTwo(root: TreeNode | null): number {
+    if (root === null) {
+        return 0
+    }
+    let treeQueue = new QueueT<TreeNode>()
+    treeQueue.push(root)
+    let number = 0;
+    while (treeQueue.size() > 0) {
+        // 队列深度即为广度
+        let size = treeQueue.size()
+        // 同一层的广度处理
+        for (let index = 0; index < size; index++) {
+            let node = treeQueue.pop();
+            if (node === undefined) {
+                continue
+            }
+            if (node.left !== null) {
+                treeQueue.push(node.left)
+            }
+            if (node.right !== null) {
+                treeQueue.push(node.right)
+            }
+        }
+        number += 1
+    }
+    return number
+};
+
+/**
+* pop的返回值当data为空时返回undefined
+* 使用了泛型约束
+*/
+class QueueT<T> {
+    private data = Array<T>();
+    size = (): number => this.data.length;
+    push = (item: T) => this.data.push(item);
+	pop = (): T | undefined => this.data.shift();
+}
+
+/**
+ * @description: 二叉树的最小深度，广度优先搜索BFS
+ * @param {TreeNode} root
+ * @return {*}
+ * BFS搜索最大深度时间复杂度和递归一样，但搜索最小深度时间复杂度明显优于DFS
+ */
+function minDepth(root: TreeNode | null): number {
+    if (root === null) {
+        return 0
+    }
+    let treeQueue = new QueueT<TreeNode>()
+    treeQueue.push(root)
+    let number = 0;
+    let search = true;
+    while (search && treeQueue.size() > 0) {
+        // 队列深度即为广度
+        let size = treeQueue.size()
+        // 同一层的广度处理
+        for (let index = 0; index < size; index++) {
+            let node = treeQueue.pop();
+            if (node === undefined) {
+                continue
+            }
+            if (node.left !== null) {
+                treeQueue.push(node.left)
+            } else {
+                search = false
+                break;
+            }
+            if (node.right !== null) {
+                treeQueue.push(node.right)
+            } else {
+                search = false
+                break;
+            }
+        }
+        number += 1
+    }
+    return number
+};
---
+++ TS/LeetCode/树/LC_617.ts
@@ -0,0 +1,49 @@
+/*
+ * @Author: tangdaoyong
+ * @Date: 2021-02-24 10:16:36
+ * @LastEditors: tangdaoyong
+ * @LastEditTime: 2021-02-24 10:30:17
+ * @Description: 617. 合并二叉树
+ */
+/*
+617. 合并二叉树
+给定两个二叉树，想象当你将它们中的一个覆盖到另一个上时，两个二叉树的一些节点便会重叠。
+
+你需要将他们合并为一个新的二叉树。合并的规则是如果两个节点重叠，那么将他们的值相加作为节点合并后的新值，否则不为 NULL 的节点将直接作为新二叉树的节点。
+*/
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+/**
+ * @description: 合并二叉树，深度优先
+ * @param {TreeNode} root1
+ * @param {TreeNode} root2
+ * @return {*}
+ * 140 ms	45.8 MB	TypeScript
+ * 复杂度分析
+时间复杂度：O(\min(m,n))O(min(m,n))，其中 mm 和 nn 分别是两个二叉树的节点个数。对两个二叉树同时进行深度优先搜索，只有当两个二叉树中的对应节点都不为空时才会对该节点进行显性合并操作，因此被访问到的节点数不会超过较小的二叉树的节点数。
+空间复杂度：O(\min(m,n))O(min(m,n))，其中 mm 和 nn 分别是两个二叉树的节点个数。空间复杂度取决于递归调用的层数，递归调用的层数不会超过较小的二叉树的最大高度，最坏情况下，二叉树的高度等于节点数。
+ */
+function mergeTrees(root1: TreeNode | null, root2: TreeNode | null): TreeNode | null {
+    if (root1 === null && root2 === null) {
+        return null
+    } else if (root1 !== null && root2 === null) {
+        return root1
+    } else if (root1 === null && root2 !== null) {
+        return root2
+    } else {
+        return new TreeNode(root1.val + root2.val, mergeTrees(root1.left, root2.left), mergeTrees(root1.right, root2.right))
+    }
+};
+
+// TODO: - 还可是使用广度优先(BFS),可参考LeetCode题解
