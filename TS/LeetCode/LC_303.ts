81df4b9ea70fa1535a27a6cdddec423a944e71f4
matiastang <18380449615@163.com>
2021-03-02 21:45:27 +0800

    feat: - LeetCode 303和304

--- TS/LeetCode/LC_303.ts
+++ TS/LeetCode/LC_303.ts
@@ -2,6 +2,85 @@
  * @Author: tangdaoyong
  * @Date: 2021-03-02 10:04:17
  * @LastEditors: tangdaoyong
- * @LastEditTime: 2021-03-02 10:04:20
- * @Description: file content
+ * @LastEditTime: 2021-03-02 17:47:21
+ * @Description: 区域和检索 - 数组不可变
+ */
+/*
+303. 区域和检索 - 数组不可变
+给定一个整数数组  nums，求出数组从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点。
+
+实现 NumArray 类：
+
+NumArray(int[] nums) 使用数组 nums 初始化对象
+int sumRange(int i, int j) 返回数组 nums 从索引 i 到 j（i ≤ j）范围内元素的总和，包含 i、j 两点（也就是 sum(nums[i], nums[i + 1], ... , nums[j])）
+ 
+
+示例：
+
+输入：
+["NumArray", "sumRange", "sumRange", "sumRange"]
+[[[-2, 0, 3, -5, 2, -1]], [0, 2], [2, 5], [0, 5]]
+输出：
+[null, 1, -1, -3]
+
+解释：
+NumArray numArray = new NumArray([-2, 0, 3, -5, 2, -1]);
+numArray.sumRange(0, 2); // return 1 ((-2) + 0 + 3)
+numArray.sumRange(2, 5); // return -1 (3 + (-5) + 2 + (-1)) 
+numArray.sumRange(0, 5); // return -3 ((-2) + 0 + 3 + (-5) + 2 + (-1))
+ 
+
+提示：
+
+0 <= nums.length <= 104
+-105 <= nums[i] <= 105
+0 <= i <= j < nums.length
+最多调用 104 次 sumRange 方法
+*/
+/**
+ * 136 ms	45.1 MB	TypeScript
+ */
+class NumArray {
+
+    /**
+     * 缓存一维前缀
+     */
+    // sums: Array<number>;//136 ms	45.1 MB	TypeScript
+    // sums = Array<number>();//144 ms	45.4 MB	TypeScript
+    sums: Array<number>//136 ms	44.8 MB	TypeScript
+    
+    constructor(nums: number[]) {
+        // let sum = 0;
+        // this.sums = Array<number>(nums.length);
+        // for (let i = 0; i < nums.length; i++) {
+        //     sum += nums[i]
+        //     this.sums[i] = sum
+        // }
+
+        // let sum = 0;
+        // for (let i = 0; i < nums.length; i++) {
+        //     sum += nums[i]
+        //     this.sums.push(sum)
+        // }
+
+        let sum = 0;
+        for (let i = 0; i < nums.length; i++) {
+            sum += nums[i]
+            nums[i] = sum
+        }
+        this.sums = nums
+    }
+
+    sumRange(i: number, j: number): number {
+        if (i === 0) {
+            return this.sums[j]
+        }
+        return this.sums[j] - this.sums[i - 1]
+    }
+}
+
+/**
+ * Your NumArray object will be instantiated and called as such:
+ * var obj = new NumArray(nums)
+ * var param_1 = obj.sumRange(i,j)
  */
--- TS/LeetCode/LC_304.ts
+++ TS/LeetCode/LC_304.ts
@@ -2,7 +2,7 @@
  * @Author: tangdaoyong
  * @Date: 2021-03-02 09:18:10
  * @LastEditors: tangdaoyong
- * @LastEditTime: 2021-03-02 10:03:59
+ * @LastEditTime: 2021-03-02 21:44:45
  * @Description: 304. 二维区域和检索 - 矩阵不可变
  */
 /*
@@ -10,7 +10,7 @@
 给定一个二维矩阵，计算其子矩形范围内元素的总和，该子矩阵的左上角为 (row1, col1) ，右下角为 (row2, col2)。
 上图子矩阵左上角 (row1, col1) = (2, 1) ，右下角(row2, col2) = (4, 3)，该子矩形内元素的总和为 8。
 */
-class NumMatrix {
+class NumMatrixOne {
 
     numberMatrix = Array<Array<number>>()
 
@@ -72,8 +72,84 @@ class NumMatrix {
  */
 /*
 * 一维前缀和
+* 120 ms	43 MB	TypeScript
 */
+class NumMatrixTwo {
+
+    numberMatrix:Array<Array<number>>
+
+    constructor(matrix: number[][]) {
+        // 改变matrix内存
+        for (let i = 0; i < matrix.length; i++) {
+            let sum = 0
+            for (let j = 0; j < matrix[i].length; j++) {
+                sum += matrix[i][j];
+                matrix[i][j] = sum
+            }
+        }
+        this.numberMatrix = matrix
+    }
+
+    /**
+     * @param row1 
+     * @param col1 
+     * @param row2 
+     * @param col2 
+     */
+    sumRegion(row1: number, col1: number, row2: number, col2: number): number {
+        let num = 0
+        for (let i = row1; i <= row2; i++) {
+            if (col1 === 0) {
+                num += this.numberMatrix[i][col2]
+            } else {
+                num += this.numberMatrix[i][col2] - this.numberMatrix[i][col1 - 1]
+            }
+        }
+        return num
+    }
+}
 
 /*
 * 二维前缀和
 */
+class NumMatrixThree {
+
+    numberMatrix = Array<Array<number>>()
+
+    constructor(matrix: number[][]) {
+        // 改变matrix内存
+        for (let i = 0; i < matrix.length; i++) {
+            for (let j = 0; j < matrix[i].length; j++) {
+                if (i === 0 && j === 0) {
+                    matrix[i][j] = matrix[i][j]
+                } else if (i === 0) {
+                    matrix[i][j] = matrix[0][j] + matrix[0][j - 1]
+                } else if (j === 0) {
+                    matrix[i][j] = matrix[i][0] + matrix[i - 1][0]
+                } else {
+                    matrix[i][j] = matrix[i][j] + matrix[i - 1][j] + matrix[i][j - 1] - matrix[i - 1][j - 1]
+                }
+            }
+        }
+        this.numberMatrix = matrix
+        console.log(matrix)
+    }
+
+    /**
+     * @param row1 
+     * @param col1 
+     * @param row2 
+     * @param col2 
+     * 116 ms	43.6 MB	TypeScript
+     */
+    sumRegion(row1: number, col1: number, row2: number, col2: number): number {
+        if (row1 === 0 && col1 === 0) {
+            return this.numberMatrix[row2][col2]
+        } else if (row1 === 0) {
+            return this.numberMatrix[row2][col2] -  this.numberMatrix[row2][col1 - 1]
+        } else if (col1 === 0) {
+            return this.numberMatrix[row2][col2] - this.numberMatrix[row1 - 1][col2]
+        }
+        return this.numberMatrix[row2][col2] - this.numberMatrix[row2][col1 - 1] - this.numberMatrix[row1 - 1][col2] + this.numberMatrix[row1 - 1][col1 - 1]
+    }
+}
