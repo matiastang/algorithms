12544575cc695da7b94e5be88350d2ba61f6e6ed
matiastang <18380449615@163.com>
2021-05-18 10:10:24 +0800

    1442. 形成两个异或相等数组的三元组数目

---
+++ TS/LeetCode/LC_1442.ts
@@ -0,0 +1,111 @@
+/*
+ * @Author: tangdaoyong
+ * @Date: 2021-05-18 09:20:19
+ * @LastEditors: tangdaoyong
+ * @LastEditTime: 2021-05-18 10:10:02
+ * @Description: 形成两个异或相等数组的三元组数目
+ */
+/*
+1442. 形成两个异或相等数组的三元组数目
+给你一个整数数组 arr 。
+
+现需要从数组中取三个下标 i、j 和 k ，其中 (0 <= i < j <= k < arr.length) 。
+
+a 和 b 定义如下：
+
+a = arr[i] ^ arr[i + 1] ^ ... ^ arr[j - 1]
+b = arr[j] ^ arr[j + 1] ^ ... ^ arr[k]
+注意：^ 表示 按位异或 操作。
+
+请返回能够令 a == b 成立的三元组 (i, j , k) 的数目。
+
+ 
+
+示例 1：
+
+输入：arr = [2,3,1,6,7]
+输出：4
+解释：满足题意的三元组分别是 (0,1,2), (0,2,2), (2,3,4) 以及 (2,4,4)
+示例 2：
+
+输入：arr = [1,1,1,1,1]
+输出：10
+示例 3：
+
+输入：arr = [2,3]
+输出：0
+示例 4：
+
+输入：arr = [1,3,5,7,9]
+输出：3
+示例 5：
+
+输入：arr = [7,11,12,9,5,2,7,17,22]
+输出：8
+ 
+
+提示：
+
+1 <= arr.length <= 300
+1 <= arr[i] <= 10^8
+*/
+function countTriplets(arr: number[]): number {
+    let dp = Array<number>(arr.length + 1).fill(0)
+    // 初始化dp数组
+    for (let i = 1; i <= arr.length; i++) {
+        dp[i] = dp[i - 1]^ arr[i]
+    }
+    // 初始化dp数组
+    let ans = 0;
+    for (let i = 0; i < arr.length; i++) {
+        for (let j = i + 1; j < arr.length; j++) {
+            for (let k = j; k < arr.length; k++) {
+                if (dp[i] === dp[k + 1]) {
+                    ans++;
+                }
+            }
+        }
+    }
+    return ans;
+};
+
+/**
+ * 
+ * @param arr 
+ * @returns 
+ * 执行用时：108 ms, 在所有 TypeScript 提交中击败了100.00%的用户
+ * 内存消耗：39.7 MB, 在所有 TypeScript 提交中击败了100.00%的用户
+ */
+function countTripletsOne(arr: number[]): number {
+    // let dp = Array<number>(arr.length).fill(0)
+    // // 初始化dp数组
+    // for (let i = 1; i <= arr.length; i++) {
+    //     dp[i] = dp[i - 1] ^ arr[i]
+    // }
+    // // 初始化dp数组
+    // let ans = 0;
+    // for (let i = 0; i < arr.length; i++) {
+    //     for (let k = i + 1; k < arr.length; k++) {
+    //         if (dp[i] === dp[k + 1]) {
+    //             ans += k - i;
+    //         }
+    //     }
+    // }
+    // return ans;
+    const n = arr.length;
+    const s = [0];
+    for (const num of arr) {
+        s.push(s[s.length - 1] ^ num);
+    }
+
+    let ans = 0;
+    for (let i = 0; i < n; i++) {
+        for (let k = i + 1; k < n; k++) {
+            if (s[i] === s[k + 1]) {
+                ans += k - i;
+            }
+        }
+    }
+
+    return ans;
+};
